42f9f6789bad5846614f442a9e8df41a6758c7e6
QcrTiMo <3192377376@qq.com>
2025-06-13 21:12:34 +0800

    update

---
+++ A. Soldier and Bananas/Program.cs
@@ -0,0 +1,48 @@
+﻿/***
+ * A. Soldier and Bananas
+time limit per test1 second
+memory limit per test256 megabytes
+A soldier wants to buy w bananas in the shop. He has to pay k dollars for the first banana, 2k dollars for the second one and so on (in other words, he has to pay i·k dollars for the i-th banana).
+
+He has n dollars. How many dollars does he have to borrow from his friend soldier to buy w bananas?
+
+Input
+The first line contains three positive integers k, n, w (1  ≤  k, w  ≤  1000, 0 ≤ n ≤ 109), the cost of the first banana, initial number of dollars the soldier has and number of bananas he wants.
+
+Output
+Output one integer — the amount of dollars that the soldier must borrow from his friend. If he doesn't have to borrow money, output 0.
+
+Examples
+Input
+3 17 4
+Output
+13
+
+ * ***/
+
+
+namespace A._Soldier_and_Bananas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ')
+                .Select(s => int.Parse(s))
+                .ToArray();
+            int k = input[0];
+            int n = input[1];
+            int w = input[2];
+            int totalCost = k * w * (w + 1) / 2;
+            if (totalCost <= n)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine(totalCost - n);
+            }
+        }
+    }
+}
---
+++ A. Word Capitalization/Program.cs
@@ -0,0 +1,48 @@
+﻿/***
+ * A. Word Capitalization
+time limit per test2 seconds
+memory limit per test256 megabytes
+Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+Note, that during capitalization all the letters except the first one remains unchanged.
+
+Input
+A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
+
+Output
+Output the given word after capitalization.
+
+Examples
+Input
+ApPLe
+Output
+ApPLe
+Input
+konjac
+Output
+Konjac
+ * ***/
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace A._Word_Capitalization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String word = Console.ReadLine();
+            char first = word[0];
+            char First = char.ToUpper(first);
+            String rest = "";
+            if (word.Length > 1)
+            {
+                rest = word.Substring(1);
+            }
+            String result = First + rest;
+            Console.WriteLine(result);
+            
+        }
+    }
+}
